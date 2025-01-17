d70274b2d6caed5c3128e51588d1d582200954ed
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-01-17 12:06:47 +0100

    indit_metódus fele kész

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,18 @@ namespace calculator
 {
 	internal class Program
 	{
+		static void indit()
+		{
+            Console.WriteLine("Kérem adja meg az első számot: ");
+			int elso = Int32.Parse(Console.ReadLine());
+			Console.WriteLine("Kérem adja meg a második számot: ");
+			int masodik = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Az elvégzendő művelet jele: ");
+			string opjel = Console.ReadLine();
+        }
 		static void Main(string[] args)
 		{
+			indit();
 		}
 	}
 }
