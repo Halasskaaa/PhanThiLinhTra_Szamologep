dbb28bfc29caec69aaab3b9a11b8a636b3f48fc1
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-01-23 13:57:52 +0100

    Osszeadas metódus kész

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -12,13 +12,22 @@ namespace calculator
 		static void indit()
 		{
             Console.WriteLine("Kérem adja meg az első számot: ");
-			int elso = Int32.Parse(Console.ReadLine());
-			Console.WriteLine("Kérem adja meg a második számot: ");
-			int masodik = Int32.Parse(Console.ReadLine());
+            double szam1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kérem adja meg a második számot: ");
+            double szam2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Az elvégzendő művelet jele: ");
 			string opjel = Console.ReadLine();
+
+            double eredmeny = Osszeadas(szam1, szam2);
+
+            Console.WriteLine("Az összeg: " + eredmeny);
         }
-		static void Main(string[] args)
+        static double Osszeadas(double szam1, double szam2)
+        {
+            return szam1 + szam2;
+        }
+
+        static void Main(string[] args)
 		{
 			indit();
 		}
